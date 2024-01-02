1148e1000e078fea781d8b13778614ace33c3eea
Mathias Niashi <69613266+niashi@users.noreply.github.com>
2024-01-02 21:44:48 +0000

    Modify the phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Jaque, te amo muito!</h1>
+    return <h1>Ohana quer dizer família. Família quer dizer nunca abandonar ou esquecer. ❤️</h1>
 }
 
 export default Home;
